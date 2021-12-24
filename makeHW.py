6b5de6ba775c2cb91f58d6b8e0faa6bc513d8fc6
Josh Jeppson <joshua.jeppson@usu.edu>
2021-12-23 20:10:14 -0700

    Should work on windows now

--- makeHW.py
+++ makeHW.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
 
+import platform
+import os
+
+# Get the filesystem delimiter (/ on linux/mac and \ on windows)
+system = platform.system()
+if system == "Linux" or system == "Darwin":
+    slash = '/'
+else:
+    slash = '\\'
+
 # Put your last name here as a "constant"
 LASTNAME="LASTNAME"
 
@@ -7,7 +17,10 @@ if __name__=='__main__':
     courseNum = input("Please input the course number:")
     assnNum = input("Please input the assignment number:")
     problemNumbers = input("Please input the problem numbers (space separated): ").split(' ')
+    print("The folder to save to is an ABSOLUTE path. To get a relative path, prefix the folder with '.'")
     folder = input("Folder to save to: ")
+    if folder[0] == '.':
+        folder = os.getcwd() + folder[1:]
     pContents = ""
     o = '{'
     c = '}'
@@ -26,5 +39,6 @@ if __name__=='__main__':
     fileContents = fileContents.replace("COURSENUMBER", courseNum)
     fileContents = fileContents.replace("ASSIGNMENTNUMBER", assnNum)
     fileContents = fileContents.replace("CONTENTS", pContents)
-    with open(f"{folder}/hw{assnNum}-{LASTNAME}.tex", "w") as outfile:
+    print(f" Saving file to:\n\t{folder}{slash}hw{assnNum}-{LASTNAME}.tex")
+    with open(f"{folder}{slash}hw{assnNum}-{LASTNAME}.tex", "w") as outfile:
         outfile.write(fileContents)
